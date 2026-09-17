5ee4ec533e3ce8953f2b6b2a0feb961e9f8bf8ba
jafrinrubaiyatjahan-cyber <jafrinrubaiyatjahan@gmail.com>
2026-09-17 12:28:24 +0900

    Update and rename sandclock to sandclock.c

---
+++ sandclock.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int main() {
+    int n;
+    int i, j;
+    scanf("%d", &n);
+    for(i = 0; i <= n / 2; i++) {
+        for(j = 0; j < i; j++) {
+            printf(" ");
+        }
+        for(j = 0; j < n - 2 * i; j++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    for(i = n / 2 - 1; i >= 0; i--) {
+        for(j = 0; j < i; j++) {
+            printf(" ");
+        }
+        for(j = 0; j < n - 2 * i; j++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
